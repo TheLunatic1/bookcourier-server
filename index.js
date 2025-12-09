59e6020aed008d9dd9ca514d8c4b0f16869da64a
Lunatic1 <112961007+TheLunatic1@users.noreply.github.com>
2025-12-10 04:59:50 +0600

    Update index.js

--- index.js
+++ index.js
@@ -45,9 +45,7 @@ app.use("/api/admin", adminRoutes);
 
 app.get("/", (req, res) => {
   res.json({ 
-    message: "BookCourier Server Running!...",
-    message: "SERVER RUNNING!...",
-    message: "MONGODB CONNECTED!...",
+    message: "BookCourier Server Running!...SERVER RUNNING!...MONGODB CONNECTED!...",
     stripe: process.env.STRIPE_SECRET_KEY ? "Connected" : "Failed"
   });
 });
